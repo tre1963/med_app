96a0f0d4888a0a27571b46dbc1915b6bd2359819
tre1963 <tre1963@hotmail.com>
2024-06-24 11:40:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from './Components/Navbar/Navbar';
+import './Components/Navbar/Navbar.css';
+import Landing_Page from './Components/LandingPage/Landing_Page';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar/>
+              <Routes>
+              <Route path="/" element={<Landing_Page />} />
+              </Routes>
+            
+        </BrowserRouter>
+       
     </div>
   );
 }
