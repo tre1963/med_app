3813cc34d791bb9c41037e345e10dc122adbe55c
tre1963 <tre1963@hotmail.com>
2024-06-26 16:08:35 -0400

    Initial commit

---
+++ src/Components/Notification/Notification.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Notification.css';
+
+const Notification = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [doctorData, setDoctorData] = useState(null);
+  const [appointmentData, setAppointmentData] = useState(null);
+
+  useEffect(() => {
+    const storedUsername = sessionStorage.getItem('email');
+    const storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
+    const storedAppointmentData = JSON.parse(localStorage.getItem(storedDoctorData?.name));
+
+    if (storedUsername) {
+      setIsLoggedIn(true);
+      setUsername(storedUsername);
+    }
+
+    if (storedDoctorData) {
+      setDoctorData(storedDoctorData);
+    }
+
+    if (storedAppointmentData) {
+      setAppointmentData(storedAppointmentData);
+    }
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      {children}
+      {isLoggedIn && appointmentData && (
+        <div className="appointment-card">
+          <div className="appointment-card__content">
+            <h3 className="appointment-card__title">Appointment Details</h3>
+            <p className="appointment-card__message">
+              <strong>User:</strong> {username.split('@')[0]} <br />
+              <strong>Doctor:</strong> {doctorData?.name} <br />
+              <strong>Appointment Date:</strong> {appointmentData.appointmentDate} <br />
+              <strong>Time Slot:</strong> {appointmentData.timeSlot}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Notification;
