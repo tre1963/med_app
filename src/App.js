6426318db2f85525b88eaa26fd07d8ddcee3d02a
tre1963 <tre1963@hotmail.com>
2024-06-26 16:25:17 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,22 +6,23 @@ import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 //import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 import BookingConsultation from './Components/BookingConsultation';
+import Notification from './Components/Notification/Notification';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing_Page />} />
-          <Route path="/Sign_Up" element={<Sign_Up />} /> {/* Example path for Sign Up */}
-          <Route path="/Login" element={<Login />} /> {/* Example path for Login */}
-         {/*<Route path="/instant-consultation" element={<InstantConsultation />} />*/} {/* Example path for Login */}
-          <Route path="/booking-consultation" element={<BookingConsultation />} /> {/* Example path for Login */}
-         
-
-
-        </Routes>
+        <Notification>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing_Page />} />
+            <Route path="/Landing_Page" element={<Landing_Page />} />
+            <Route path="/Sign_Up" element={<Sign_Up />} />
+            <Route path="/Login" element={<Login />} />
+            {/* <Route path="/instant-consultation" element={<InstantConsultation />} /> */}
+            <Route path="/booking-consultation" element={<BookingConsultation />} />
+          </Routes>
+        </Notification>
       </BrowserRouter>
     </div>
   );
